7b269ac2000acfae952fa4253e7acd63df1b228f
Christian7573 <cag4561@gmail.com>
2020-08-24 21:00:12 -0500

    Got to step 2 in selecting items in RecyclerView

--- app/src/main/java/net/blazetek/duotest/QuestionList.kt
+++ app/src/main/java/net/blazetek/duotest/QuestionList.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.selection.ItemDetailsLookup
+import androidx.recyclerview.selection.ItemKeyProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -45,7 +48,9 @@ class QuestionListAdapter(activity: QuizActivity) : RecyclerView.Adapter<Questio
         val view = LayoutInflater.from(activity).inflate(R.layout.fragment_question_list_item, parent, false);
         view.findViewById<TextView>(R.id.question_list_item_header).text = question_number.toString() + ": " + activity.quiz.questions[question_number].question;
         view.findViewById<TextView>(R.id.question_list_item_content).text = "Unanswered"; //quiz.questions.get(question_number).question;
-        return QuestionListAdapterViewHolder(view);
+        val holder = QuestionListAdapterViewHolder(question_number, view);
+        question_number += 1;
+        return holder
     }
 
     override fun onBindViewHolder(holder: QuestionListAdapterViewHolder, position: Int) {
@@ -53,4 +58,24 @@ class QuestionListAdapter(activity: QuizActivity) : RecyclerView.Adapter<Questio
     }
     override fun getItemCount() = activity.quiz.questions.count();
 }
-class QuestionListAdapterViewHolder(layout: View) : RecyclerView.ViewHolder(layout)
+class QuestionListAdapterViewHolder(key: Int, layout: View) : RecyclerView.ViewHolder(layout) {
+    val key = key;
+}
+class QuestionListAdapterItemKeyProvider : ItemKeyProvider<Int>(ItemKeyProvider.SCOPE_MAPPED) {
+    override fun getKey(position: Int) = position;
+    override fun getPosition(key: Int) = key;
+}
+class QuestionListAdapterItemDetalisLookup(recycler: RecyclerView) : ItemDetailsLookup<Int>() {
+    val recycler = recycler;
+    override fun getItemDetails(e: MotionEvent): ItemDetails<Int>? {
+        val motioned_over = recycler.findChildViewUnder(e.x, e.y);
+        if (motioned_over != null) {
+            val holder = recycler.getChildViewHolder(motioned_over) as QuestionListAdapterViewHolder;
+            return object : ItemDetails<Int>() {
+                override fun getPosition(): Int = holder.key
+                override fun getSelectionKey(): Int? = holder.key
+            }
+        }
+        return null;
+    }
+}
