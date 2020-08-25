9874830d54aedb5685f2374656f1ff5441e5c1ba
Christian7573 <cag4561@gmail.com>
2020-08-24 20:32:09 -0500

    Working on Question List

--- app/src/main/java/net/blazetek/duotest/QuestionList.kt
+++ app/src/main/java/net/blazetek/duotest/QuestionList.kt
@@ -1,10 +1,15 @@
 package net.blazetek.duotest
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +22,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class QuestionList : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question_list, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QuestionList.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QuestionList().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val view = inflater.inflate(R.layout.fragment_question_list, container, false) as RecyclerView
+        view.setHasFixedSize(true);
+        view.layoutManager = LinearLayoutManager(context)
+        view.adapter = QuestionListAdapter(activity as QuizActivity)
+        return view
     }
 }
+
+class QuestionListAdapter(activity: QuizActivity) : RecyclerView.Adapter<QuestionListAdapterViewHolder>()  {
+    val activity = activity;
+    var question_number = 0;
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): QuestionListAdapterViewHolder {
+        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_question_list_item, parent, false);
+        view.findViewById<TextView>(R.id.question_list_item_header).text = question_number.toString() + ": " + activity.quiz.questions[question_number].question;
+        view.findViewById<TextView>(R.id.question_list_item_content).text = "Unanswered"; //quiz.questions.get(question_number).question;
+        return QuestionListAdapterViewHolder(view);
+    }
+
+    override fun onBindViewHolder(holder: QuestionListAdapterViewHolder, position: Int) {
+        //lmao no
+    }
+    override fun getItemCount() = activity.quiz.questions.count();
+}
+class QuestionListAdapterViewHolder(layout: View) : RecyclerView.ViewHolder(layout)
--- app/src/main/java/net/blazetek/duotest/QuizActivity.kt
+++ app/src/main/java/net/blazetek/duotest/QuizActivity.kt
@@ -1,9 +1,13 @@
 package net.blazetek.duotest
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.fragment.app.FragmentActivity
 import com.microsoft.fluentui.appbarlayout.AppBarLayout
 
@@ -11,7 +15,6 @@ class QuizActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        println("Not okee dokee");
         //val appbar: AppBarLayout = findViewById(R.id.quiz_appbar);
         gesture_detector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
             override fun onFling(
@@ -26,6 +29,23 @@ class QuizActivity : FragmentActivity() {
                 } else return false;
             }
         });
+    }
+
+    lateinit var quiz: Quiz;
+    var selected_question = 0;
+    var selected_answers = HashMap<Int, Int>();
+
+    override fun onStart() {
+        super.onStart();
+        intent.data!!.host!!.toInt();
+        val my_quiz: Quiz? = quizi.get(intent.data!!.host!!.toInt());
+        if (my_quiz == null) {
+            Toast.makeText(this, "Unable to open quiz: Invalid URL", Toast.LENGTH_SHORT);
+            startActivity(Intent(Intent.ACTION_MAIN));
+            return;
+        }
+        quiz = my_quiz;
+        findViewById<Toolbar>(R.id.quiz_topbar).title = quiz.name;
     }
 
     var gesture_detector: GestureDetector? = null;
